9f0ab5fdbbcb599ebc5d69d089ab651328c5e83b
BMasterton <ICS21-126@camosun.bc.ca>
2024-03-22 14:37:43 -0700

    Initial Commit

---
+++ Lab9GameUITests/Lab9GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab9GameUITestsLaunchTests.swift
+//  Lab9GameUITests
+//
+//  Created by IMD 224 on 2024-03-22.
+//
+
+import XCTest
+
+final class Lab9GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
